1461d0e6036737aec8657a1ba90d271cc13c3c95
dongduu <4012popo@naver.com>
2022-01-14 21:23:37 +0900

    feat: webpack setting 숫자야구

---
+++ 숫자야구/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+
+const WordRelay = require("./NumberBaseBall");
+
+ReactDom.render(<NumberBaseBall />, document.querySelector("#root"));
