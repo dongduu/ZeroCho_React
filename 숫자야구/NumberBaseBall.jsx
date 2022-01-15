8f4d3bf742c8f1d59a3e7bcc5c23da09cfe294d3
dongduu <4012popo@naver.com>
2022-01-15 23:36:24 +0900

    feat: practice map

--- 숫자야구/NumberBaseBall.jsx
+++ 숫자야구/NumberBaseBall.jsx
@@ -1,6 +1,48 @@
 const React = require("react");
 const { Component } = React;
 
-class NumberBaseBall {}
+function getNumbers() {}
+
+class NumberBaseBall extends Component {
+  state = {
+    result: "",
+    value: "",
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmitForm = (e) => {
+    e.preventDefault();
+  };
+
+  onChangeInput = (e) => {
+    this.state.value = e.target.value;
+  };
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            value={this.state.value}
+            type="string"
+            onChange={this.onChangeInput}
+            maxLength={4}
+          />
+          <button>입력</button>
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {
+            (["배", "바나나", "사과"].map = (v) => {
+              return <li>{v}</li>;
+            })
+          }
+        </ul>
+      </>
+    );
+  }
+}
 
 module.exports = NumberBaseBall;
