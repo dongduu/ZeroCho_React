e5682327929f5b7396c24313ca5c4a6a0db27cae
dongduu <4012popo@naver.com>
2022-01-17 15:42:24 +0900

    feat: 숫자야구 (class component)

--- 숫자야구/NumberBaseBall.jsx
+++ 숫자야구/NumberBaseBall.jsx
@@ -1,8 +1,16 @@
 const React = require("react");
 const { Component } = React;
-import Try from "./Try";
+const Try = require("./Try");
 
-function getNumbers() {}
+function getNumbers() {
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i++) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
 
 class NumberBaseBall extends Component {
   state = {
@@ -14,17 +22,62 @@ class NumberBaseBall extends Component {
 
   onSubmitForm = (e) => {
     e.preventDefault();
+    if (this.state.value === this.state.answer.join("")) {
+      this.setState({
+        result: "홈런!",
+        tries: [
+          ...this.state.tries,
+          { try: this.state.value, result: "홈런!" },
+        ],
+      });
+      alert("게임을 다시 시작합니다.");
+      this.setState({
+        value: "",
+        answer: getNumbers(),
+        tries: [],
+      });
+    } else {
+      const answerArray = this.state.value.split("").map((v) => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.tries.length >= 9) {
+        this.setState({
+          result: `실패! 답은 ${this.state.answer.join(",")}였습니다.`,
+        });
+        alert("게임을 다시 시작합니다.");
+        this.setState({
+          value: "",
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i++) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          tries: [
+            ...this.state.tries,
+            {
+              try: this.state.value,
+              result: `${strike} 스트라이크, ${ball} 볼입니다!`,
+            },
+          ],
+          value: "",
+        });
+      }
+    }
   };
 
   onChangeInput = (e) => {
-    this.state.value = e.target.value;
+    console.log(this.state.answer);
+    this.setState({
+      value: e.target.value,
+    });
   };
-
-  fruits = [
-    { fruit: "배", taste: "맛없다" },
-    { fruit: "바나나", taste: "맛있다" },
-    { fruit: "사과", taste: "달다" },
-  ];
 
   render() {
     return (
@@ -33,16 +86,16 @@ class NumberBaseBall extends Component {
         <form onSubmit={this.onSubmitForm}>
           <input
             value={this.state.value}
-            type="string"
             onChange={this.onChangeInput}
             maxLength={4}
+            type="number"
           />
           <button>입력</button>
         </form>
         <div>시도: {this.state.tries.length}</div>
         <ul>
-          {this.fruits.map((v, i) => {
-            return <Try value={v} index={i} />;
+          {this.state.tries.map((v, i) => {
+            return <Try key={`${i + 1}차 시도: `} tryInfo={v} />;
           })}
         </ul>
       </>
--- 숫자야구/Try.js
+++ 숫자야구/Try.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+const React = require("react");
+const { Component } = React;
 
 class Try extends Component {
   render() {
     return (
       <li>
-        <b>{this.props.value.fruit}</b> - {this.props.index}
-        <div>컨텐츠1</div>
-        <div>컨텐츠2</div>
-        <div>컨텐츠3</div>
-        <div>컨텐츠4</div>
+        <div>{this.props.tryInfo.try}</div>
+        <div>{this.props.tryInfo.result}</div>
       </li>
     );
   }
 }
 
-export default Try;
+module.exports = Try;
