162fe640bbbb0c81cb5130344f176069d60da8f5
dongduu <4012popo@naver.com>
2022-01-17 13:48:12 +0900

    feat: practice props

--- 숫자야구/NumberBaseBall.jsx
+++ 숫자야구/NumberBaseBall.jsx
@@ -1,5 +1,6 @@
 const React = require("react");
 const { Component } = React;
+import Try from "./Try";
 
 function getNumbers() {}
 
@@ -19,6 +20,12 @@ class NumberBaseBall extends Component {
     this.state.value = e.target.value;
   };
 
+  fruits = [
+    { fruit: "배", taste: "맛없다" },
+    { fruit: "바나나", taste: "맛있다" },
+    { fruit: "사과", taste: "달다" },
+  ];
+
   render() {
     return (
       <>
@@ -34,24 +41,9 @@ class NumberBaseBall extends Component {
         </form>
         <div>시도: {this.state.tries.length}</div>
         <ul>
-          {[
-            { fruit: "배", taste: "맛없다" },
-            { fruit: "바나나", taste: "맛있다" },
-            { fruit: "사과", taste: "달다" },
-          ].map((v) => (
-            <li key={v.fruit + v.taste}>
-              <b>{v.fruit}</b> - {v.taste}
-            </li>
-          ))}
-          {/* <li>
-            <b>배</b> - 맛없다
-          </li>
-          <li>
-            <b>바나나</b> - 맛있다
-          </li>
-          <li>
-            <b>사과</b> - 달다
-          </li> */}
+          {this.fruits.map((v, i) => {
+            return <Try value={v} index={i} />;
+          })}
         </ul>
       </>
     );
