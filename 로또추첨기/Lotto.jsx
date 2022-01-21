915e8a34faf85ff18872ae50f9317e3a04bd9288
dongduu <4012popo@naver.com>
2022-01-21 17:12:26 +0900

    delete 로또추첨기

--- 로또추첨기/Lotto.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Lotto extends Component {
-  render() {
-    return <div>로또</div>;
-  }
-}
-
-export default Lotto;
--- 로또추첨기/client.jsx
+++
@@ -1,6 +0,0 @@
-const React = require("react");
-const ReactDom = require("react-dom");
-
-const Lotto = require("./Lotto");
-
-ReactDom.render(<Lotto />, document.querySelector("#root"));
