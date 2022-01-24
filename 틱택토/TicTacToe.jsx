535f523ba0ea8d30e459e92d2e3337e7e02c0f85
dongduu <4012popo@naver.com>
2022-01-24 17:38:07 +0900

    feat: reducer, dispatch, action

--- 틱택토/TicTacToe.jsx
+++ 틱택토/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useCallback } from "react";
 import Table from "./Table";
 
 const initialState = {
@@ -11,23 +11,34 @@ const initialState = {
   ],
 };
 
-const reducer = (state, action) => {};
+const SET_WINNER = "SET_WINNER";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_WINNER:
+      // state.winner = action.winner <= 이렇게 하면 안됨
+      return {
+        ...state,
+        winner: action.winner,
+      };
+  }
+};
 
 const TicTacToe = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   //   const [winner, setWinner] = useState("");
   //   const [turn, setTurn] = useState("O");
-  //   const [tableData, setTableDate] = useState([
-  //     ["", "", ""],
-  //     ["", "", ""],
-  //     ["", "", ""],
-  //   ]);
+  //   const [tableData, setTableDate] = useState([["", "", ""], ["", "", ""], ["", "", ""]]);
+
+  const onClickEvent = useCallback(() => {
+    dispatch({ type: "SET_WINNER", winner: "o" });
+  }, []);
 
   return (
     <>
-      <Table />
-      {winner && <div>{winner}님의 승리</div>}
+      <Table onClick={onClickEvent} tableData={state.tableData} />
+      {state.winner && <div>{state.winner}님의 승리</div>}
     </>
   );
 };
