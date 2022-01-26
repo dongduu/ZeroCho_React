5be2f3dab7feb525eca066bb60716422814df8c9
dongduu <4012popo@naver.com>
2022-01-26 14:14:49 +0900

    feat: add createContext, provider

--- 지뢰찾기/MineSearch.jsx
+++ 지뢰찾기/MineSearch.jsx
@@ -1,5 +1,22 @@
-import React, { useReducer } from "react";
+import React, { useReducer, createContext, useMemo } from "react";
 import Form from "./Form";
+import Table from "./Table";
+
+export const CODE = {
+  MINE: -7,
+  NOMAL: -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICK_MINE: -6,
+  OPENED: 0,
+};
+
+export const TableContext = createContext({
+  tableData: [],
+  dispatch: () => {},
+});
 
 const initialState = {
   tableData: [],
@@ -7,8 +24,15 @@ const initialState = {
   result: "",
 };
 
+export const START_GAME = "START_GAME";
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
     default:
       return state;
   }
@@ -17,13 +41,18 @@ const reducer = (state, action) => {
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const value = useMemo(
+    () => ({ tableData: state.tableData, dispatch }),
+    [state.tableData]
+  );
+
   return (
-    <>
+    <TableContext.Provider value={value}>
       <Form />
       <div>{state.timer}</div>
       <Table />
       <div>{state.result}</div>
-    </>
+    </TableContext.Provider>
   );
 };
 
